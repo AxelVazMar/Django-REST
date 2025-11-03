63681cf7e473b4f4e929b5f9d51db8fabc1524bb
Axel Vazquez <axelvazmar0@gmail.com>
2025-11-02 22:41:05 -0600

    Kwoning about api_views and adding a view to list one product filtering by the id

--- api/serializers.py
+++ api/serializers.py
@@ -5,8 +5,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
+            "id",
             "name",
-            #"description",
             "price",
             "stock",
         )
--- api/views.py
+++ api/views.py
@@ -1,10 +1,19 @@
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': serializer.data # .data is the representation of the data of the content serializer variable
-    })
+    return Response(serializer.data)
+
+# View to list 1 product filtering by the id
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk) # how this is a single object we don't need a many=true
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
